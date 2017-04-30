dd1f3f2b02bfdd2c670f63aacd286a35ae5cad12
bharathsai <bharathsai007@gmail.com>
2017-04-30 15:51:19 +0530

    You tube upload changes

---
+++ src/main/java/com/bharath/YoutubeIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.bharath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeIntegrationApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(YoutubeIntegrationApplication.class, args);
+	}
+}
